64bb497084c03adf585b1bbfc1b3321e99036783
elzup <guild0105@gmail.com>
2023-01-18 16:38:51 +0900

    add view

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use nannou::prelude::*;
+use nannou::{prelude::*};
 
 fn main() {
+    nannou::sketch(view_without_model).run();
     nannou::app(model)
         .update(update)
         .simple_window(view)
@@ -8,15 +9,29 @@ fn main() {
         .run()
 }
 
-struct Model {}
+struct Model {
+    color: Srgb<u8>,
+}
 
 fn model(_app: &App) -> Model {
-    Model {}
+    Model { color: RED }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
 }
 
 fn view(_app: &App, _model: &Model, frame: Frame){
-    frame.clear(PURPLE);
+    let draw = _app.draw();
+
+    draw.background().color(_model.color);
+    draw.to_frame(_app, &frame).unwrap();
+
+}
+
+fn view_without_model(_app: &App, frame: Frame){
+    let draw = _app.draw();
+
+    draw.background().color(BLUE);
+    draw.to_frame(_app, &frame).unwrap();
+
 }
